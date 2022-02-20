ac729d7d0167f15d7cb50ac27450be69a49b0932
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2022-02-20 13:37:06 +1100

    Copied layout from mikro-orm + nestjs example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { DoorsModule } from './doors/doors.module';
 import { HealthModule } from './health/health.module';
 import { AutomationHatModule } from './automation-hat/automation-hat.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { MikroOrmMiddleware, MikroOrmModule } from '@mikro-orm/nestjs';
+import { MikroORM } from '@mikro-orm/core';
+import { DatabaseSeeder } from './seeders/DatabaseSeeder';
 
 @Module({
   imports: [
@@ -18,4 +20,18 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly orm: MikroORM) {}
+
+  async onModuleInit(): Promise<void> {
+    await this.orm.getMigrator().up();
+    await this.orm.getSeeder().seed(DatabaseSeeder);
+  }
+
+  // for some reason the auth middlewares in profile and article modules are fired before the request context one,
+  // so they would fail to access contextual EM. by registering the middleware directly in AppModule, we can get
+  // around this issue
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer.apply(MikroOrmMiddleware).forRoutes('*');
+  // }
+}
--- src/doors/doors.module.ts
+++ src/doors/doors.module.ts
@@ -4,7 +4,7 @@ import { AuthModule } from '../auth/auth.module';
 import { AutomationHatModule } from '../automation-hat/automation-hat.module';
 import { DoorsController } from './doors.controller';
 import { DoorsService } from './doors.service';
-import { Door } from '../entities/Door';
+import { Door } from '../entities/Door.entity';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Door]), AutomationHatModule, AuthModule],
