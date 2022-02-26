a2115098173cee4afc1057d7d9fbce99ce85a316
michaelgambold <7439294+michaelgambold@users.noreply.github.com>
2022-02-26 17:55:40 +1100

    fix: Seed script using only relay 1 (#11)
    * feat: Promote Door.id to auto increment PK
    
    * fix: Wrong relays in seed script

--- src/seeders/DatabaseSeeder.ts
+++ src/seeders/DatabaseSeeder.ts
@@ -4,26 +4,61 @@ import { Door } from '../entities/Door.entity';
 
 export class DatabaseSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    const ids = [1, 2, 3];
+    const door1 = await em.findOne(Door, 1);
 
-    for (const id of ids) {
-      // check if record exists already
-      const existingDoor = await em.findOne(Door, id);
-
-      if (existingDoor) continue;
-
+    if (!door1) {
       em.create(Door, {
-        id,
-        label: `Door ${id}`,
+        id: 1,
+        label: `Door 1`,
         isEnabled: true,
         state: 'closed',
         sequences: [
           {
             action: 'on',
-            door: id,
+            door: 1,
             duration: 1000,
             index: 1,
             target: 'relay1',
+          },
+        ],
+      });
+    }
+
+    const door2 = await em.findOne(Door, 2);
+
+    if (!door2) {
+      em.create(Door, {
+        id: 2,
+        label: `Door 2`,
+        isEnabled: true,
+        state: 'closed',
+        sequences: [
+          {
+            action: 'on',
+            door: 2,
+            duration: 1000,
+            index: 1,
+            target: 'relay2',
+          },
+        ],
+      });
+    }
+
+    const door3 = await em.findOne(Door, 3);
+
+    if (!door3) {
+      em.create(Door, {
+        id: 3,
+        label: `Door 3`,
+        isEnabled: true,
+        state: 'closed',
+        sequences: [
+          {
+            action: 'on',
+            door: 3,
+            duration: 1000,
+            index: 1,
+            target: 'relay3',
           },
         ],
       });
