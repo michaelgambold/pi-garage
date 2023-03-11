b1579d29594cb4288e11ea4a93e3f42a0b626ebd
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2023-03-12 07:57:57 +1100

    🚧 Skeleton local storage service tests

---
+++ app/test/services/local_storage_service_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pi_garage/services/local_storage_service.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  late LocalStorageService service;
+
+  setUp(() {
+    WidgetsFlutterBinding.ensureInitialized();
+    service = LocalStorageService.instance;
+  });
+
+  // test("Contains Key",
+  //     () async => {expect(await service.containsKey('key'), equals(false))});
+
+  // test(
+  //     "Get boolean value",
+  //     () async =>
+  //         {expect(await service.getBooleanValue('boolean-value'), false)});
+}
