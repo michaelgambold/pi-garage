b1ee6fe0c5024b98ae4ed421669f559139745692
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2023-03-26 16:25:17 +1100

    ✨ Add config selector to menu drawer

--- app/lib/widgets/menu_drawer.dart
+++ app/lib/widgets/menu_drawer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pi_garage/models/config.dart';
+import 'package:pi_garage/repositories/config_repository.dart';
+import 'package:pi_garage/widgets/config_dropdown.dart';
 
 import '../services/app_version_service.dart';
 
@@ -11,9 +14,12 @@ class MenuDrawer extends StatefulWidget {
 
 class _MenuDrawerState extends State<MenuDrawer> {
   final _appVersionService = AppVersionService();
+  final _configRepository = ConfigRepository();
   final _appVersionTextSize = 12.0;
 
   String _appVersion = '';
+  List<Config> _configs = [];
+  Config? _currentConfig;
 
   @override
   void initState() {
@@ -22,6 +28,22 @@ class _MenuDrawerState extends State<MenuDrawer> {
     _appVersionService.getAppVersion().then((value) => setState(() {
           _appVersion = value;
         }));
+    _configRepository.findAllConfigs().then((value) => setState(() {
+          _configs = value;
+        }));
+    _refreshCurrentConfig();
+  }
+
+  Future<void> _refreshCurrentConfig() async {
+    final currentConfig = await _configRepository.findCurrentConfig();
+    setState(() {
+      _currentConfig = currentConfig;
+    });
+  }
+
+  Future<void> _selectConfig(String value) async {
+    await _configRepository.selectConfig(value);
+    await _refreshCurrentConfig();
   }
 
   @override
@@ -37,14 +59,18 @@ class _MenuDrawerState extends State<MenuDrawer> {
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Expanded(
-                        child: Text(
-                      "Menu",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                      ),
-                    )),
+                    Expanded(
+                        child: Column(children: [
+                      const Text("Menu",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                          )),
+                      ConfigDropdown(
+                          configs: _configs,
+                          currentConfigId: _currentConfig?.id,
+                          onChange: (value) => _selectConfig(value))
+                    ])),
                     SizedBox(
                         height: _appVersionTextSize,
                         child: Text(
