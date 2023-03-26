7b8b1102c25625fb93e6f3157145e163e9d0c97b
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2023-03-26 16:08:26 +1100

    ✨ Add update all configs method

--- app/lib/repositories/config_repository.dart
+++ app/lib/repositories/config_repository.dart
@@ -1,21 +1,16 @@
 import 'dart:convert';
 import 'package:pi_garage/models/config.dart';
 import 'package:pi_garage/services/local_storage_service.dart';
-import 'package:uuid/uuid.dart';
 
 class ConfigRepository {
   Future<void> addConfig(Config config) async {
     var configs = await findAllConfigs();
     configs.add(config);
-    await _saveAll(configs);
+    await updateAllConfigs(configs);
   }
 
   Future<List<Config>> findAllConfigs() async {
     final localStorageService = LocalStorageService.instance;
-
-    // perform migration of single to multiple configs if required
-    await _migrateSingleToMultipleConfig();
-
     final configStrings =
         await localStorageService.getStringListValue('configs');
 
@@ -39,7 +34,7 @@ class ConfigRepository {
   Future<void> removeConfig(String id) async {
     final configs = await findAllConfigs();
     configs.removeWhere((element) => element.id == id);
-    await _saveAll(configs);
+    await updateAllConfigs(configs);
   }
 
   Future<void> selectConfig(String id) async {
@@ -47,42 +42,17 @@ class ConfigRepository {
     await localStorageService.setStringValue('currentConfigId', id);
   }
 
-  Future<void> updateConfig(Config config) async {
-    final configs = await findAllConfigs();
-    final index = configs.indexWhere((element) => element.id == config.id);
-    configs[index] = config;
-    await _saveAll(configs);
-  }
-
-  Future<void> _saveAll(List<Config> configs) async {
+  Future<void> updateAllConfigs(List<Config> configs) async {
     final jsonStrings = configs.map((e) => json.encode(e.toJson())).toList();
 
     final localStorageService = LocalStorageService.instance;
     await localStorageService.setStringListValue('configs', jsonStrings);
   }
 
-  // multiple configs introduced 1.8.0 (25 Mar), keep until TO_BE_DECIDED
-  Future<void> _migrateSingleToMultipleConfig() async {
-    final localStorage = LocalStorageService.instance;
-
-    // if we already have new configs defined just skip migration
-    if (await localStorage.containsKey('configs')) {
-      return;
-    }
-
-    // extract all old config items
-    final fqdn = await localStorage.getStringValue('global_fqdn');
-    final apiKey = await localStorage.getStringValue('global_api_key');
-
-    // create new config item
-    const uuid = Uuid();
-    final config = Config(uuid.v4(), "default", fqdn, apiKey);
-
-    await _saveAll([config]);
-    await selectConfig(config.id);
-
-    // remove old config items
-    await localStorage.removeValue('global_fqdn');
-    await localStorage.removeValue('global_api_key');
+  Future<void> updateConfig(Config config) async {
+    final configs = await findAllConfigs();
+    final index = configs.indexWhere((element) => element.id == config.id);
+    configs[index] = config;
+    await updateAllConfigs(configs);
   }
 }
