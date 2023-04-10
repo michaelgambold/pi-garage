af8021ec61f465b56380dc9218f4212735a78548
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2023-04-10 17:20:27 +1000

    ✨ Create current config provider

---
+++ app/lib/providers/current_config_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+
+import '../models/config.dart';
+import '../repositories/config_repository.dart';
+
+class CurrentConfigProvider extends ChangeNotifier {
+  Config? _currentConfig;
+
+  Config? get currentConfig => _currentConfig;
+
+  CurrentConfigProvider() {
+    final configRepo = ConfigRepository();
+    configRepo.findCurrentConfig().then((value) {
+      _currentConfig = value;
+      notifyListeners();
+    });
+  }
+
+  setCurrentConfig(String id) async {
+    final configRepo = ConfigRepository();
+    await configRepo.setCurrentConfig(id);
+    _currentConfig = await configRepo.findCurrentConfig();
+    notifyListeners();
+  }
+}
