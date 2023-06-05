7ad9f15b2b49c6ffe8f9003c0af294487e546a75
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2023-06-05 11:59:14 +1000

    ♻️ Replace delay with inbuilt await setTimout

--- service/src/health/health.controller.ts
+++ service/src/health/health.controller.ts
@@ -1,8 +1,8 @@
 import { ConsoleLogger, Controller, Get, LoggerService } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
-import delay from 'delay';
 import { AutomationHatService } from '../automation-hat/automation-hat.service';
 import { HealthDto } from './dto/health.dto';
+import { setTimeout } from 'timers/promises';
 
 @Controller('health')
 export class HealthController {
@@ -20,7 +20,7 @@ export class HealthController {
     // try to read value from automation hat. we don't care on the value
     // but this should crash if we cannot read it
     this.automationHatService.turnOnCommsLight();
-    await delay(50);
+    await setTimeout(50);
     this.automationHatService.turnOffCommsLight();
 
     return {
