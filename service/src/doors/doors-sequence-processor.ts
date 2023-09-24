d2cebaffab40e876a37ee63752a066cc45926c0b
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2023-09-24 15:53:53 +1000

    🚧 Basic working of door sequence processor

--- service/src/doors/doors-sequence-processor.ts
+++ service/src/doors/doors-sequence-processor.ts
@@ -2,12 +2,20 @@ import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { DoorQueue, DoorsSequenceQueueMessage } from './types';
 import { Job } from 'bullmq';
 import { ConsoleLogger, LoggerService } from '@nestjs/common';
+import { DoorsService } from './doors.service';
+import { EntityManager } from '@mikro-orm/core';
+// import { Door } from '../entities/Door.entity';
+// import { InjectRepository } from '@mikro-orm/nestjs';
+import { setTimeout } from 'timers/promises';
 
 @Processor(DoorQueue.DOORS_SEQUENCE_RUN)
 export class DoorsSequenceProcessor extends WorkerHost {
   #logger: LoggerService;
 
-  constructor() {
+  constructor(
+    private readonly em: EntityManager,
+    private readonly doorsService: DoorsService,
+  ) {
     super();
     this.#logger = new ConsoleLogger(DoorsSequenceProcessor.name);
   }
@@ -15,24 +23,39 @@ export class DoorsSequenceProcessor extends WorkerHost {
   async process(
     job: Job<DoorsSequenceQueueMessage, void, string>,
   ): Promise<void> {
-    // push message to change the doors state in x time
+    // const door = await this.doorsService.findOne(job.data.doorId);
+    // console.log(door);
 
-    switch (job.name) {
-      case 'close':
-        this.#logger.log('received close door message');
-        // await this.doorsService.close(id);
-        break;
-      case 'open':
-        this.#logger.log('received close open message');
-        // await this.doorsService.open(id);
-        break;
-      case 'toggle':
-        this.#logger.log('received close toggle message');
-        // await this.doorsService.toggle(id);
-        break;
-      default:
-        this.#logger.warn(`Invalid job name: ${job.name}`);
-    }
+    const context = this.em.fork();
+
+    await context.transactional(async () => {
+      const door = await this.doorsService.findOne(job.data.doorId);
+      this.#logger.log(JSON.stringify(door));
+
+      // in the future get the door duration from config but for now hard code it
+      const duration = 2000;
+
+      // send message to queue to change state of door async or should we do this in the one transaction space?
+
+      switch (job.name) {
+        case 'close':
+          this.#logger.log('received close door message');
+          // await this.doorsService.close(id);
+          break;
+        case 'open':
+          this.#logger.log('received close open message');
+          // await this.doorsService.open(id);
+          break;
+        case 'toggle':
+          this.#logger.log('received close toggle message');
+          // await this.doorsService.toggle(id);
+          break;
+        default:
+          this.#logger.warn(`Invalid job name: ${job.name}`);
+      }
+
+      await setTimeout(duration);
+    });
 
     console.log(job.name, job.data);
   }
