750d98575827b0758f8f4a3c8add65c37180cff3
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2023-10-07 22:45:32 +1100

    ✅ Get single test working without async issue

--- service/src/doors/doors-sequence-processor.spec.ts
+++ service/src/doors/doors-sequence-processor.spec.ts
@@ -5,47 +5,121 @@ import { EntityManager } from '@mikro-orm/sqlite';
 import { MikroORM } from '@mikro-orm/core';
 import { AutomationHatService } from '../automation-hat/automation-hat.service';
 import { ConfigService } from '@nestjs/config';
+import { Job, Queue } from 'bullmq';
+import { DoorSequenceJobName, DoorsSequenceJobData } from './types';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { SequenceObject } from '../entities/SequenceObject.entity';
 
 describe('DoorsSequenceProcessor', () => {
   let provider: DoorsSequenceProcessor;
-  // let doorsService: DoorsService;
-  // let entityManager: EntityManager;
+  let doorsService: DoorsService;
+  let automationHatService: AutomationHatService;
+  let testQueue: Queue;
+  let orm: MikroORM;
+
+  beforeAll(() => {
+    // testQueue = new Queue('test-queue', {});
+  });
+
+  afterAll(async () => {
+    // await testQueue.close();
+  });
 
   beforeEach(async () => {
     const mockDoorsService = {
-      get: jest.fn().mockResolvedValue({}),
+      findOne: jest.fn().mockResolvedValue({
+        sequence: [
+          {
+            action: 'on',
+            duration: 1000,
+            id: 1,
+            index: 1,
+            target: 'relay1',
+          },
+        ],
+      }),
     };
 
     const mockEntityManager = {
       get: jest.fn(),
     };
 
+    const mockAutomationHatService = {
+      runSequenceObject: jest.fn().mockResolvedValue(undefined),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
+      // imports: [MikroOrmModule.forRoot()],
       providers: [
         DoorsSequenceProcessor,
-        AutomationHatService,
         ConfigService,
         {
           provide: MikroORM,
-          useValue: {},
+          useValue: MikroORM.init(),
         },
-        {
-          provide: EntityManager,
-          useValue: mockEntityManager,
-        },
+        // {
+        //   provide: EntityManager,
+        //   useValue: mockEntityManager,
+        // },
         {
           provide: DoorsService,
           useValue: mockDoorsService,
+        },
+        {
+          provide: AutomationHatService,
+          useValue: mockAutomationHatService,
         },
       ],
     }).compile();
 
     provider = module.get<DoorsSequenceProcessor>(DoorsSequenceProcessor);
-    // doorsService = module.get<DoorsService>(DoorsService);
+    doorsService = module.get<DoorsService>(DoorsService);
+    automationHatService =
+      module.get<AutomationHatService>(AutomationHatService);
+    orm = module.get<MikroORM>(MikroORM);
     // entityManager = module.get<EntityManager>(EntityManager);
+  });
+
+  afterEach(async () => {
+    await orm.close();
   });
 
   it('should be defined', () => {
     expect(provider).toBeDefined();
+  });
+
+  it('should process door open job', async () => {
+    const doorsServiceSpy = jest.spyOn(doorsService, 'findOne');
+    const automationHatServiceSpy = jest.spyOn(
+      automationHatService,
+      'runSequenceObject',
+    );
+    const expectedSequenceObj = {
+      action: 'on',
+      duration: 1000,
+      id: 1,
+      index: 1,
+      target: 'relay1',
+    };
+
+    const job = {
+      name: DoorSequenceJobName.OPEN,
+      data: {
+        doorId: 1,
+      },
+    };
+
+    await provider.process(job as Job);
+
+    expect(doorsServiceSpy).toBeCalledWith(1);
+    expect(automationHatServiceSpy).toBeCalledWith(expectedSequenceObj);
+  });
+
+  it('should process door close job', async () => {
+    // todo
+  });
+
+  it('should ignore jobs it does not know about', async () => {
+    // todo
   });
 });
