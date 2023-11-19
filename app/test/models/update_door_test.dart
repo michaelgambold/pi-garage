9fd314c56c19361698cb5ee6336649c6c2262c19
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2023-11-19 21:23:19 +1100

    ✅ Update tests to have open/close duration

--- app/test/models/update_door_test.dart
+++ app/test/models/update_door_test.dart
@@ -3,10 +3,21 @@ import 'package:pi_garage/models/update_door.dart';
 
 void main() {
   test('To JSON', () {
-    const updateDoor = UpdateDoor(label: 'Door Label', isEnabled: true);
+    const updateDoor = UpdateDoor(
+        label: 'Door Label',
+        isEnabled: true,
+        openDuration: 20000,
+        closeDuration: 20000);
 
     final json = updateDoor.toJson();
 
-    expect(json, equals({'label': 'Door Label', 'isEnabled': true}));
+    expect(
+        json,
+        equals({
+          'label': 'Door Label',
+          'isEnabled': true,
+          "openDuration": 20000,
+          "closeDuration": 20000
+        }));
   });
 }
