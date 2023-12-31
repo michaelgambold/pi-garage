21ebd51327568f5ccb09c3f90bef167e48b1c87d
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2023-12-31 15:09:51 +1100

    ✨ Add default timout to all API requests

--- app/lib/services/api_service.dart
+++ app/lib/services/api_service.dart
@@ -5,6 +5,8 @@ import '../repositories/config_repository.dart';
 import 'app_version_service.dart';
 import 'http_service.dart';
 
+const defaultTimeout = Duration(seconds: 5);
+
 class ApiService {
   final _httpService = HttpService();
   final _configRepo = ConfigRepository();
@@ -14,29 +16,34 @@ class ApiService {
     final config = await _configRepo.findCurrentConfig();
     final uri = Uri.parse('${config.fqdn}$path');
     final headers = await _getHeaders(config);
-    return _httpService.get(uri, headers);
+
+    return _httpService.get(uri, headers).timeout(defaultTimeout);
   }
 
   Future<Response> post(String path, Object? body) async {
     final config = await _configRepo.findCurrentConfig();
     final uri = Uri.parse('${config.fqdn}$path');
     final headers = await _getHeaders(config);
-    return _httpService.post(uri, body, headers);
+
+    return _httpService.post(uri, body, headers).timeout(defaultTimeout);
   }
 
   Future<Response> put(String path, Object? body) async {
     final config = await _configRepo.findCurrentConfig();
     final uri = Uri.parse('${config.fqdn}$path');
     final headers = await _getHeaders(config);
-    return _httpService.put(uri, body, headers);
+
+    return _httpService.put(uri, body, headers).timeout(defaultTimeout);
   }
 
   _getHeaders(Config config) async {
     final clientVersion = await _appVersionService.getAppVersion();
 
     final headers = <String, String>{};
-    headers.addAll(
-        {'x-api-key': config.apiKey ?? "", 'x-client-version': clientVersion});
+    headers.addAll({
+      'x-api-key': config.apiKey ?? "",
+      'x-client-version': clientVersion,
+    });
     return headers;
   }
 }
