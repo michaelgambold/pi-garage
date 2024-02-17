b797406be8a186d7a2bd0b2963c9d084b500af3b
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2024-02-18 10:11:05 +1100

    ✨ Use module init to connect/disconnect to redis

--- service/src/doors/doors-lock.service.spec.ts
+++ service/src/doors/doors-lock.service.spec.ts
@@ -1,18 +1,62 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+// import { Redis } from 'ioredis';
+// import RedisMock from 'ioredis-mock';
 import { DoorsLockService } from './doors-lock.service';
 
 describe('DoorsLockService', () => {
+  let module: TestingModule;
   let service: DoorsLockService;
+  const doorId = 1;
+
+  // let redis;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [DoorsLockService],
+    module = await Test.createTestingModule({
+      providers: [DoorsLockService, ConfigService],
     }).compile();
 
+    await module.init();
+
     service = module.get<DoorsLockService>(DoorsLockService);
+
+    await service.unlockDoor(doorId);
+    // const config = module.get<ConfigService>(ConfigService);
+
+    // redis = new RedisMock({
+    //   host: config.get('REDIS_HOST'),
+    //   port: config.get('REDIS_PORT'),
+    // });
+
+    // Redis.mockImplementation();
+    // await module.init();
   });
 
-  it('should be defined', () => {
+  afterEach(async () => {
+    await module.close();
+  });
+
+  it('should be defined', async () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be unlocked when missing from redis', async () => {
+    const doorLocked = await service.isLocked(doorId);
+    expect(doorLocked).toEqual(false);
+  });
+
+  it('should be able to lock door', async () => {
+    await service.lockDoor(doorId);
+
+    const doorLocked = await service.isLocked(1);
+    expect(doorLocked).toEqual(true);
+  });
+
+  it('should be able to unlock door', async () => {
+    await service.lockDoor(doorId);
+    await service.unlockDoor(doorId);
+
+    const doorLocked = await service.isLocked(doorId);
+    expect(doorLocked).toEqual(false);
   });
 });
--- service/src/doors/doors-lock.service.ts
+++ service/src/doors/doors-lock.service.ts
@@ -1,20 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Redis } from 'ioredis';
+import { Logger } from '../logger/logger';
 
 const DOOR_LOCK_KEY_TTL = 60; // in seconds
 const DOOR_LOCKED_VALUE = 'locked';
 
 @Injectable()
-export class DoorsLockService {
-  private readonly redisClient: Redis;
+export class DoorsLockService implements OnModuleInit, OnModuleDestroy {
+  private readonly logger: Logger;
+  private redisClient: Redis;
 
   constructor(private readonly config: ConfigService) {
+    this.logger = new Logger(DoorsLockService.name);
+  }
+
+  async onModuleInit() {
     this.redisClient = new Redis({
-      host: config.get('REDIS_HOST'),
-      port: config.get('REDIS_PORT'),
+      host: this.config.get('REDIS_HOST'),
+      port: this.config.get('REDIS_PORT'),
       keyPrefix: 'lock',
+    }).on('error', (error) => {
+      console.log(error);
+
+      this.logger.error(error);
     });
+  }
+
+  onModuleDestroy() {
+    this.redisClient.disconnect();
   }
 
   private getKey(doorId: number): string {
@@ -23,7 +37,6 @@ export class DoorsLockService {
 
   async isLocked(doorId: number): Promise<boolean> {
     const key = this.getKey(doorId);
-
     const count = await this.redisClient.exists(key);
 
     if (count === 1) {
@@ -35,11 +48,13 @@ export class DoorsLockService {
   }
 
   async lockDoor(doorId: number): Promise<void> {
+    this.logger.log(`Locking door ${doorId}`);
     const key = this.getKey(doorId);
     await this.redisClient.setex(key, DOOR_LOCK_KEY_TTL, DOOR_LOCKED_VALUE);
   }
 
   async unlockDoor(doorId: number): Promise<void> {
+    this.logger.log(`Unlocking door ${doorId}`);
     const key = this.getKey(doorId);
     await this.redisClient.del(key);
   }
