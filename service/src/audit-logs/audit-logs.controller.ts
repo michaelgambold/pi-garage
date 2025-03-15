05c2a22d8c6e163699e31f803577ade046add188
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2025-03-15 15:25:51 +0800

    📝 Update OpenAPI swagger

--- service/src/audit-logs/audit-logs.controller.ts
+++ service/src/audit-logs/audit-logs.controller.ts
@@ -1,16 +1,31 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiSecurity } from '@nestjs/swagger';
+import {
+  ApiOkResponse,
+  ApiResponse,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
 import { HttpApiKeyAuthGuard } from '../auth/http-api-key-auth.guard';
 import { HttpClientVersionGuard } from '../client-version/http-client-version.guard';
 import { AuditLogsService } from './audit-logs.service';
 import { AuditLogDto } from './dto/audit-log.dto';
+import { AuditLog } from '../entities/AuditLog.entity';
 
-@UseGuards(HttpClientVersionGuard, HttpApiKeyAuthGuard)
 @ApiSecurity('api-key')
+@ApiTags('audit-logs')
+@UseGuards(HttpClientVersionGuard, HttpApiKeyAuthGuard)
 @Controller('api/v1/audit-logs')
 export class AuditLogsController {
   constructor(private readonly auditLogsService: AuditLogsService) {}
 
+  @ApiOkResponse({
+    type: AuditLogDto,
+    isArray: true,
+    example: [
+      { timestamp: '2025-03-16T10:38:20.000Z', detail: 'Door 1 Open' },
+      { timestamp: '2025-03-16T10:38:00.000Z', detail: 'Door 1 Opening' },
+    ],
+  })
   @Get()
   async getAll(): Promise<AuditLogDto[]> {
     const auditLogs = await this.auditLogsService.findAll();
